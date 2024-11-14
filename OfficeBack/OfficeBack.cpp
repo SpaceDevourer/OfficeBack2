4e2a2b793700f97459086fc6498147599ebb7e6f
centr <centr@LAPTOP-1G7COFAG>
2024-11-15 02:34:37 +0800

    Добавьте файлы проекта.

---
+++ OfficeBack/OfficeBack.cpp
@@ -0,0 +1,22 @@
+﻿VisualStudioVersion = 17.8.34322.80
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "BackEnd", "BackEnd\BackEnd.csproj", "{E4C9A7F5-0A3C-49CF-91CE-D7870D1471C4}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug | Any CPU = Debug | Any CPU
+		Release | Any CPU = Release | Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{ E4C9A7F5 - 0A3C - 49CF - 91CE - D7870D1471C4 }.Debug | Any CPU.ActiveCfg = Debug | Any CPU
+		{ E4C9A7F5 - 0A3C - 49CF - 91CE - D7870D1471C4 }.Debug | Any CPU.Build.0 = Debug | Any CPU
+		{ E4C9A7F5 - 0A3C - 49CF - 91CE - D7870D1471C4 }.Release | Any CPU.ActiveCfg = Release | Any CPU
+		{ E4C9A7F5 - 0A3C - 49CF - 91CE - D7870D1471C4 }.Release | Any CPU.Build.0 = Release | Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = { 85A6DBF7 - 4032 - 406A - 90EF - 49CB85B9802F }
+	EndGlobalSection
+EndGlobal
